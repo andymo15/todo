c4323ce626ffaf653ab50634f881fbb6ef45b66a
Andy <anndymo@gmail.com>
2023-01-03 16:44:51 -0800

    added skeleton routes on backend

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,28 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json())
 
+
 app.get("/getData", (req,res)=>{
   res.send("Hello World")
+});
+
+app.get("/todos", (req,res)=>{
+  // use Todo.find to find all todos
 })
+
+app.post("/todos", (req,res)=>{
+  // add db.Todo.create
+})
+
+app.put("todos/:id", (req,res)=>{
+  // find todo by ID and edit it
+})
+
+app.delete("todos/:id", (req,res)=>{
+  // find todo by ID and delete it
+})
+
+
 
 app.listen(5000, ()=>
 console.log("app is running on Port 5000"))
